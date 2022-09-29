d2e37c44906535a6f89637199271123a09fb5727
Sebastian Vargas <33919190+sebas-tcotd@users.noreply.github.com>
2022-09-28 20:12:45 -0500

    Rename card.js to Card.js

--- src/components/card/QrCode.js
+++ src/components/card/QrCode.js
@@ -2,5 +2,5 @@ import React from "react";
 import qrImage from "./image-qr-code.png";
 
 export const QrCode = () => {
-  return <img src={qrImage} alt="QR Image" className="c-card__head" />;
+  return <img src={qrImage} alt="QR code" className="c-card__head" />;
 };
